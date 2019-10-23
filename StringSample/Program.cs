4bf2735602b75128efc6a1fd82f77389cd77b2a1
lyt <numb327@gmail.com>
2019-10-23 09:25:50 +0800

    string sample

---
+++ StringSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
